743ec5f69aabb0c891a72fe7df5159251aeca39c
MaxMelnik1311 <maaxfresh1100@gmail.com>
2020-02-19 02:24:32 +0200

    addedFirstTryToPassHW

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import shortid from 'shortid';
+import { Notyf } from 'notyf';
+import 'notyf/notyf.min.css';
+import ContactForm from './Components/ContactForm/ContactForm';
+import ContactList from './Components/ContactList/ContactList';
+import Filter from './Components/Filter/Filter';
+
+export default class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  addContact = (name, number) => {
+    const notyf = new Notyf();
+    const contact = {
+      id: shortid.generate(),
+      name,
+      number,
+    };
+    const findAddedContact = (contacts, person) =>
+      contacts.find(item => item.name === person);
+
+    if (findAddedContact(this.state.contacts, contact.name)) {
+      return notyf.error(`${contact.name} is already exists in contacts!`);
+    }
+    this.setState(prevState => {
+      return {
+        contacts: [...prevState.contacts, contact],
+      };
+    });
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(({ id }) => id !== contactId),
+      };
+    });
+  };
+
+  changeFilter = filter => {
+    this.setState({ filter });
+  };
+
+  getVisibleContacts = () => {
+    const { contacts, filter } = this.state;
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase()),
+    );
+  };
+
+  render() {
+    const { filter } = this.state;
+    const visibleContacts = this.getVisibleContacts();
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm submitContactInfo={this.addContact} />
+        <h2>Contacts</h2>
+
+        <Filter value={filter} onChangeFilter={this.changeFilter} />
+        <ContactList
+          list={visibleContacts}
+          deleteContact={this.deleteContact}
+        />
+      </div>
+    );
+  }
+}
